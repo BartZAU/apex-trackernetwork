059fc32ba88eecfadfc59ad18456c0e0df6dd650
Bart Zabielski <bart.zabielski@DCS-G2YR6QVMP6-bart.zabielski>
2023-03-14 06:59:11 +1100

    refactor

--- routes/profile.js
+++ routes/profile.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-router.get("/:platform/:gamertag", async (req, res) => {
+router.get("/:platform/:steamId", async (req, res) => {
   try {
     const headers = {
       "TRN-Api-Key": process.env.TRACKER_API_KEY,
     };
 
-    const { platform, gamertag } = req.params;
+    const { platform, steamId } = req.params;
 
     const response = await fetch(
-      `${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`,
+      `${process.env.TRACKER_API_URL}/profile/${platform}/${steamId}`,
       {
         headers,
       }
